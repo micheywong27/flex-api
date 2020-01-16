319229fab63761a029dc9eb368c11c96377af4ea
hdp2112 <hdp2112@columbia.edu>
2020-01-16 17:26:31 -0500

    added CRUD functionality to API

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,4 +5,34 @@ class PostsController < ApplicationController
         render json: posts, status: :ok
     end 
 
+    def show
+        post = Post.find(params[:id])
+        render json: post, status: :ok
+    end
+
+    def create
+        post = Post.new(post_params)
+        if post.save
+            render json: post, status: :ok
+        else
+            render json: post.errors, status: :unprocessable_entity
+    end
+
+    def update
+        post = Post.find(params[:id])
+        if post.update_attributes(post_params)
+            render json: post, status: :ok
+        else
+            render json: post.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        post = Post.find(params[:id])
+        post.destroy
+        render json: post, status: :ok
+    end
+
+    private
+
 end
